fe0c5a23e27807d05183d67795438f24736e3adb
Harvey Woo <harvey.woo@outlook.com>
2019-08-01 17:40:37 +0800

    fix: fix cache error not correct

--- src/precache-image.js
+++ src/precache-image.js
@@ -1,11 +1,15 @@
 const imagePrecachePromise = {}
-  
+
 export function precacheImage(url) {
   return imagePrecachePromise[url] = imagePrecachePromise[url] || new Promise((res, rej) => {
     const img = new Image()
     img.onload = () => res(url)
-    img.onerror = rej
+    img.onerror = () => {
+      const error = new Error(`Can\'t not load image: ${url}, please validate provided url and check network status`)
+      rej(error)
+    }
     img.src = url
   })
 }
+
 export default precacheImage
